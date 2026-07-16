cb95f2560580244ccec66ec1a6299e6a12e92d52
Francisco Delgado <pacho104@hotmail.com>
2026-07-16 16:04:17 -0500

    se define el evento click y los colores aleatorios en RBG

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,14 @@
 /*==Definir los elementos a seleccionar==*/
 const parrafo = document.getElementById('parrafo');
 const btnGenerar = document.getElementById('cambiarBtn');
+
+/*====Se define el evento clik===*/
+btnGenerar.addEventListener('click', function () {
+    let colorR = Math.floor(Math.random() * 256);
+    let colorG = Math.floor(Math.random() * 256);
+    let colorB = Math.floor(Math.random() * 256);
+
+
+
+});
+
